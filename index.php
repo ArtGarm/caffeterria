d7100fdd3a4693bb5201ebf1672178074fa5af8a
Dmitry <dmitry1940@mail.ru>
2015-10-23 14:55:35 +0300

    markup index

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-	<div class="main mbox">
-		<section class="content_w">
-			<div class="content">
-				<?partial('testcontent')?>
-			</div>
-		</section>
-		<aside class="sidebar_w">
-			<div class="sidebar"></div>
-		</aside>
-		<div class="clear"></div>
+	<div class="main">
+		<? partial('slider'); ?>
+
+		<? partial('massonry'); ?>
+
+		<? partial('special'); ?>
+
+		<? partial('about-us'); ?>
+
 	</div>
