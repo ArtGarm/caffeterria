44409cbfbc8a2419f3e8f1d6c4a93262b295c72f
trolizmaslom <trolizmaslom@gmail.com>
2015-10-23 16:10:08 +0300

    special fix from troll

---
+++ partials/special.php
@@ -0,0 +1,11 @@
+<div class="mbox">
+    <div class="special-wrapper">
+        <div class="special-align">
+            <div class="special-title"><span>специальные предложения</span></div>
+            <div class="img-wrapp">
+                <div class="img-menu"><img src="images/new-menu.jpg" alt=""></div>
+                <div class="img-tort"><img src="images/tort.jpg" alt=""></div>
+            </div>
+        </div>
+    </div>
+</div>
