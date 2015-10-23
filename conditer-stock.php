1ed848415f219dfe3c2d03f8db664481e590d7de
Dmitry <dmitry1940@mail.ru>
2015-10-23 15:27:42 +0300

    added all pages

---
+++ conditer-stock.php
@@ -0,0 +1,23 @@
+<div class="main">
+
+    <? partial('conditer-stock-title'); ?>
+
+    <div class="blur-wrap">
+
+        <div class="breadcrumbs">
+
+        </div>
+
+        <div class="title">
+
+        </div>
+
+        <? partial('conditer-stock-text'); ?>
+
+    </div>
+
+    <? partial('items-conditer-stock'); ?>
+
+    <? partial('special'); ?>
+
+</div>
---
+++ contacts.php
@@ -0,0 +1,17 @@
+    <div class="main">
+
+        <? partial('about-title'); ?>
+
+        <div class="breadcrumbs">
+
+        </div>
+
+        <div class="title">
+
+        </div>
+
+        <? partial('contacts-content'); ?>
+
+        <? partial('special'); ?>
+
+    </div>
