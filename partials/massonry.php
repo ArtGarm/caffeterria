cd13ab85fc294d2d429a0abdbf8c83d91bc596d5
Radiks <radion91@gmail.com>
2015-10-23 18:00:51 +0200

    mas 70%

--- partials/massonry.php
+++ partials/massonry.php
@@ -12,11 +12,31 @@
                             <span>Хит</span>
                         </div>
                     </div>
+                    <div class="massonry_item_text_desc">
+                        Кофейные макаруны
+                    </div>
+                    <div class="massonry_item_text_subdesc">
+                        39&nbsp;Р&nbsp;<span>/1 шт</span>
+                    </div>
                 </div>
             </div>
             <div class="massonry_item item_2">
-                <div class="massonry_item_img ">
+                <div class="massonry_item_img img-centered">
                     <img src="images/massonry_item_img_2.png" alt="">
+                        <div class="img-centered-text">
+                            <p>Спешите!</p>
+                            <p>огромные скидки</p>
+                        </div>
+                </div>
+                <div class="massonry_item_text">
+                    <div class="massonry_item_text_descnotsample">
+                        <p>Скидки 50% на всю продукцию с содержанием яблока в честь</p>
+                        <p>праздника "Яблочный Спас". Успей насладиться вкуснейшими </p>
+                        <p>десертами и витаминами.</p>
+                    </div>
+                    <a href="#" class="look_and_buy">
+                        <span>Посмотреть и купить</span>
+                    </a>
                 </div>
             </div>
             <div class="massonry_item item_3">
@@ -32,6 +52,17 @@
             <div class="massonry_item item_5">
                 <div class="massonry_item_img">
                     <img src="images/massonry_item_img_5.png" alt="">
+                </div>
+                <div class="red_circle">
+                    <div class="red_circle_wrap">
+                        <span>NEW</span>
+                    </div>
+                </div>
+                <div class="massonry_item_text_desc">
+                    Пирожное шамони
+                </div>
+                <div class="massonry_item_text_subdesc">
+                    89&nbsp;Р&nbsp;<span>/1 шт</span>
                 </div>
             </div>
             <div class="massonry_item item_6">
