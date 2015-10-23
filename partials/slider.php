be60d1aa612905487d619eca9a191130509910c1
ArtGarm <artem.garmatyk@gmail.com>
2015-10-23 17:58:29 +0300

    slider + fonts

---
+++ partials/slider.php
@@ -0,0 +1,59 @@
+
+    <div class="header-slider-wrap">
+        <div class="header-slider-wrap-main">
+
+            <div class="header-slider-item">
+                <div class="conteiner-content">
+                    <div class="content-box-white">
+                        <h1>el WILD  <span> CHERRY </span></h1>
+                        <h2>Нежный сливочный бисквит c шоколадной крошкой, с воздушным суфле <br /> и дикой аргентинской вишней.</h2>
+                        <h3> 134 ГР. / 92 ККАЛ </h3>
+                        <a href="#">  <div class="button-fast"> быстрая покупка </div></a>
+                        <div class="counter"> 1 </div>
+
+                    </div>
+                    <img src="images/slider-image1.jpg" alt="" />
+                </div>
+            </div>
+            <div class="header-slider-item">
+                <div class="conteiner-content">
+                    <div class="content-box-black">
+                        <h1>Chocolate  <span> latte </span></h1>
+                        <h2>Нежный сливочный бисквит c шоколадной крошкой, с воздушным суфле и дикой аргентинской вишней.</h2>
+                        <h3> 134 ГР. / 92 ККАЛ </h3>
+                        <a href="#">  <div class="button-fast"> быстрая покупка </div></a>
+                        <div class="counter">  </div>
+                    </div>
+                    <img src="images/slider-image2.jpg" alt="" />
+                </div>
+            </div>
+            <div class="header-slider-item">
+                <div class="conteiner-content">
+                    <div class="content-box-white">
+                        <h1>el WILD  <span> CHERRY </span></h1>
+                        <h2>Нежный сливочный бисквит c шоколадной крошкой, с воздушным суфле и дикой аргентинской вишней.</h2>
+                        <h3> 134 ГР. / 92 ККАЛ </h3>
+                        <a href="#">  <div class="button-fast"> быстрая покупка </div></a>
+                        <div class="counter">  </div>
+                    </div>
+                    <img src="images/slider-image1.jpg" alt="" />
+                </div>
+            </div>
+            <div class="header-slider-item">
+                <div class="conteiner-content">
+                    <div class="content-box-black">
+                        <h1>Chocolate   <span> latte  </span></h1>
+                        <h2>Нежный сливочный бисквит c шоколадной крошкой, с воздушным суфле и дикой аргентинской вишней.</h2>
+                        <h3> 134 ГР. / 92 ККАЛ </h3>
+                        <a href="#">  <div class="button-fast"> быстрая покупка </div></a>
+                        <div class="counter">  </div>
+                    </div>
+                    <img src="images/slider-image2.jpg" alt="" />
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
