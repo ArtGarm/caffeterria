04163603f5702c6199cd49a784add2af027528a2
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-10-26 10:43:27 +0200

    no message

---
+++ partials/action-content.php
@@ -0,0 +1,18 @@
+<div class="action-content-wrap">
+	<div class="mbox">
+		<div class="action-content">
+			<div class="action-block action1">
+				<h2>Три Булки</h2>
+				<p>каждая третья булка в подарок</p>
+			</div>
+			<div class="action-block action2">
+				<h2>Ягодный бум!</h2>
+				<p><span>скидка 80%</span> на все ягодные продукты</p>
+			</div>
+			<div class="action-block action3">
+				<h2>Yyyy Macarons</h2>
+				<p>сколько покупаешь, столько получаешь в подарок</p>
+			</div>
+		</div>
+	</div>
+</div>
