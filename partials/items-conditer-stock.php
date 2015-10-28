d3cff7f462dba058bccfcd8fbbb79a7851ef402d
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-10-28 15:58:29 +0200

    no message

---
+++ partials/items-conditer-stock.php
@@ -0,0 +1,254 @@
+<div class="catalog-content-wrap conditer-stock-page">
+    <div class="mbox">
+        <div class="catalog-content">
+            <div class="catalog-item-title">
+                <span>торты в наличие</span>
+            </div>
+            <div class="catalog-items cfix">
+                <div class="row3">
+                    <div class="catalog-item oneHeight large-item">
+                        <div class="catalog-item-img">
+                            <div class="catalog-item-img-wrap">
+                                <a href="images/stock-item1.jpg" class="loop">
+                                    <img src="images/stock-item1.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="catalog-item-text">
+                            <div class="catalog-item-desc">
+                                <p>изысканый торт колибри</p>
+                            </div>
+                            <div class="catalog-item-price">
+                                <p><b>3 200 P</b> / 3350 <span>гр.</span></p>
+                            </div>
+                        </div>
+                        <div class="add-to-cart">
+                            <a href="#catalog_pop" class="catalog_pop">в корзину</a>
+                        </div>
+                    </div>
+                    <div class="catalog-item oneHeight">
+                        <div class="catalog-item-img">
+                            <div class="catalog-item-img-wrap">
+                                <a href="images/stock-item2.jpg" class="loop">
+                                    <img src="images/stock-item2.jpg" alt=""/>
+                                </a>
+                            </div>
+                            <div class="red_circle">
+                                <div class="red_circle_wrap">
+                                    <span>New</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="catalog-item-text">
+                            <div class="catalog-item-desc">
+                                <p>голый вишневый торт</p>
+                            </div>
+                            <div class="catalog-item-price">
+                                <p><b>2 599 P</b> / 1100 <span>гр.</span></p>
+                            </div>
+                        </div>
+                        <div class="add-to-cart">
+                            <a href="#catalog_pop" class="catalog_pop">в корзину</a>
+                        </div>
+                    </div>
+                    <div class="catalog-item oneHeight">
+                        <div class="catalog-item-img">
+                            <div class="catalog-item-img-wrap">
+                                <a href="images/stock-item3.jpg" class="loop">
+                                    <img src="images/stock-item3.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="catalog-item-text">
+                            <div class="catalog-item-desc">
+                                <p>печенье с мускатным орехом</p>
+                            </div>
+                            <div class="catalog-item-price">
+                                <p><b>1 229 P</b> / 1000 <span>гр.</span></p>
+                            </div>
+                        </div>
+                        <div class="add-to-cart">
+                            <a href="#catalog_pop" class="catalog_pop">в корзину</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row4">
+                    <div class="catalog-item oneHeight">
+                        <div class="catalog-item-img">
+                            <div class="catalog-item-img-wrap">
+                                <a href="images/stock-item4.jpg" class="loop">
+                                    <img src="images/stock-item4.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="catalog-item-text">
+                            <div class="catalog-item-desc">
+                                <p>торталетки асорти</p>
+                            </div>
+                            <div class="catalog-item-price">
+                                <p><b>2 450 P</b> / 1000 <span>гр.</span></p>
+                            </div>
+                        </div>
+                        <div class="add-to-cart">
+                            <a href="#catalog_pop" class="catalog_pop">в корзину</a>
+                        </div>
+                    </div>
+                    <div class="catalog-item oneHeight">
+                        <div class="catalog-item-img">
+                            <div class="catalog-item-img-wrap">
+                                <a href="images/stock-item5.jpg" class="loop">
+                                    <img src="images/stock-item5.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="catalog-item-text">
+                            <div class="catalog-item-desc">
+                                <p>клубничный бум</p>
+                            </div>
+                            <div class="catalog-item-price">
+                                <p><b>3 600 P</b> / 2900 <span>гр.</span></p>
+                            </div>
+                        </div>
+                        <div class="add-to-cart">
+                            <a href="#catalog_pop" class="catalog_pop">в корзину</a>
+                        </div>
+                    </div>
+                    <div class="catalog-item oneHeight">
+                        <div class="catalog-item-img">
+                            <div class="catalog-item-img-wrap">
+                                <a href="images/stock-item6.jpg" class="loop">
+                                    <img src="images/stock-item6.jpg" alt=""/>
+                                </a>
+                            </div>
+                            <div class="red_circle">
+                                <div class="red_circle_wrap">
+                                    <span>20%</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="catalog-item-text">
+                            <div class="catalog-item-desc">
+                                <p>калинка малинка</p>
+                            </div>
+                            <div class="catalog-item-price">
+                                <p><b>1 879 P</b> / 2400 <span>гр.</span></p>
+                            </div>
+                        </div>
+                        <div class="add-to-cart">
+                            <a href="#catalog_pop" class="catalog_pop">в корзину</a>
+                        </div>
+                    </div>
+                    <div class="catalog-item oneHeight">
+                        <div class="catalog-item-img">
+                            <div class="catalog-item-img-wrap">
+                                <a href="images/stock-item7.jpg" class="loop">
+                                    <img src="images/stock-item7.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="catalog-item-text">
+                            <div class="catalog-item-desc">
+                                <p>малиновое фрапе</p>
+                            </div>
+                            <div class="catalog-item-price">
+                                <p><b>5 200 P</b> / 1000 <span>гр.</span></p>
+                            </div>
+                        </div>
+                        <div class="add-to-cart">
+                            <a href="#catalog_pop" class="catalog_pop">в корзину</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row4">
+                    <div class="catalog-item oneHeight">
+                        <div class="catalog-item-img">
+                            <div class="catalog-item-img-wrap">
+                                <a href="images/stock-item9.jpg" class="loop">
+                                    <img src="images/stock-item9.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="catalog-item-text">
+                            <div class="catalog-item-desc">
+                                <p>трюфельный лес</p>
+                            </div>
+                            <div class="catalog-item-price">
+                                <p><b>1 279 P</b> / 1500 <span>гр.</span></p>
+                            </div>
+                        </div>
+                        <div class="add-to-cart">
+                            <a href="#catalog_pop" class="catalog_pop">в корзину</a>
+                        </div>
+                    </div>
+                    <div class="catalog-item oneHeight">
+                        <div class="catalog-item-img">
+                            <div class="catalog-item-img-wrap">
+                                <a href="images/stock-item9.jpg" class="loop">
+                                    <img src="images/stock-item9.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="catalog-item-text">
+                            <div class="catalog-item-desc">
+                                <p>Лесной ежик</p>
+                            </div>
+                            <div class="catalog-item-price">
+                                <p><b>2 399 P</b> / 2100 <span>гр.</span></p>
+                            </div>
+                        </div>
+                        <div class="add-to-cart">
+                            <a href="#catalog_pop" class="catalog_pop">в корзину</a>
+                        </div>
+                    </div>
+                    <div class="catalog-item oneHeight">
+                        <div class="catalog-item-img">
+                            <div class="catalog-item-img-wrap">
+                                <a href="images/stock-item10.jpg" class="loop">
+                                    <img src="images/stock-item10.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="catalog-item-text">
+                            <div class="catalog-item-desc">
+                                <p>happy birthday</p>
+                            </div>
+                            <div class="catalog-item-price">
+                                <p><b>3 279 P</b> / 3100 <span>гр.</span></p>
+                            </div>
+                        </div>
+                        <div class="add-to-cart">
+                            <a href="#catalog_pop" class="catalog_pop">в корзину</a>
+                        </div>
+                    </div>
+                    <div class="catalog-item oneHeight">
+                        <div class="catalog-item-img">
+                            <div class="catalog-item-img-wrap">
+                                <a href="images/stock-item10.jpg" class="loop">
+                                    <img src="images/stock-item10.jpg" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="catalog-item-text">
+                            <div class="catalog-item-desc">
+                                <p>ягодный бум</p>
+                            </div>
+                            <div class="catalog-item-price">
+                                <p><b>2 399 P</b> / 1500 <span>гр.</span></p>
+                            </div>
+                        </div>
+                        <div class="add-to-cart">
+                            <a href="#catalog_pop" class="catalog_pop">в корзину</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="load-more-items">
+                <div class="load-more-wrap cfix">
+                    <div class="load-more">
+                        <a href="" data-page="malina">Показать ещё</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
