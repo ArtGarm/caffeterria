14a651842b4713aec7f1dcc72806b2e1d7bebed2
trolizmaslom <trolizmaslom@gmail.com>
2015-10-30 10:25:50 +0200

    fix

--- send.php
+++ send.php
@@ -11,7 +11,7 @@
         </div>
         <div class="title-photo send">
             <div class="title-photo-align">
-                <div class="bread-img"><img src="images/braed-send.jpg" alt=""></div>
+                <div class="bread-img"><div class="bread-img-wrapp"><img src="images/braed-send.jpg" alt=""></div></div>
                 <div class="bread-text"><span>Для заказа доставки из сети кафе-кондитерских Кафетеррия вам необходимо сделать заказ ONLINE, дождаться его подтверждения по телефону, при возникновении вопросов вы можете связаться с нашими менеджерами по телефону</span></div>
                 <div class="bread-phone"><span>+7 (499) 653-72-72</span></div>
                 <div class="bred-min"><span>Минимальная сумма заказа составляет</span></div>
