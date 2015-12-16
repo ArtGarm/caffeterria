f473ee376b3e1a412d8ac6dadc7c7606d6f95294
trolizmaslom <trolizmaslom@gmail.com>
2015-12-16 15:34:28 +0200

    special  + about us + footer -- all adaptive

--- partials/about-us.php
+++ partials/about-us.php
@@ -1,14 +1,18 @@
-<div class="mbox">
-    <div class="about-us-wrapper">
-        <div class="about-us-align">
-            <div class="about-title"><span>уютное кафе в центре москвы "кафетеррия"</span></div>
-            <div class="img-wrapp">
-                <a href="#"><img src="images/about.jpg" alt=""></a>
-
-            </div>
-            <div class="about-text">
-                <span>Caffeterria — это молодой, развивающийся сетевой бренд. В наших кафе Caffeterria мы предлагаем Вам концепцию здорового, недорогого общественного питания  с использованием натуральных продуктов. У нас всегда широкий выбор хлебобулочных, кондитерских изделий собственного производства, а также большой выбор сэндвичей, салатов и горячих блюд.
-                </span>
+<div class="about-us">
+    <div class="mbox troll">
+        <div class="about-us-wrapper">
+            <div class="about-us-align">
+                <div class="about-title"><span>уютное кафе в центре москвы «кафетеррия»</span></div>
+                <div class="img-wrapp">
+                    <a href="#">
+                        <img src="images/about.jpg" alt="">
+                        <div class="about-logo"><img src="images/aboutlogo.png" width="301" height="55"alt=""></div>
+                    </a>
+                </div>
+                <div class="about-text">
+                    <span>Caffeterria — это молодой, развивающийся сетевой бренд. В наших кафе Caffeterria мы предлагаем Вам концепцию здорового, недорогого общественного питания  с использованием натуральных продуктов. У нас всегда широкий выбор хлебобулочных, кондитерских изделий собственного производства, а также большой выбор сэндвичей, салатов и горячих блюд.
+                    </span>
+                </div>
             </div>
         </div>
     </div>
--- partials/special.php
+++ partials/special.php
@@ -1,10 +1,12 @@
-<div class="mbox">
-    <div class="special-wrapper">
-        <div class="special-align">
-            <div class="special-title"><span>специальные предложения</span></div>
-            <div class="img-wrapp">
-                <div class="img-menu img-drop"><a href="#"><img src="images/new-menu.jpg" alt=""></a></div>
-                <div class="img-tort img-drop"><a href="#"><img src="images/tort.jpg" alt=""></a></div>
+<div class="special">
+    <div class="mbox">
+        <div class="special-wrapper">
+            <div class="special-align">
+                <div class="special-title"><span>специальные предложения</span></div>
+                <div class="img-wrapp cfix">
+                    <div class="img-menu img-drop"><a href="#"><img src="images/new-menu.jpg" alt=""></a></div>
+                    <div class="img-tort img-drop"><a href="#"><img src="images/tort.jpg" alt=""></a></div>
+                </div>
             </div>
         </div>
     </div>
